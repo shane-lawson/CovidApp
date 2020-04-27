75a78446a6031f0c3d92b9d48494320b4f29aa3c
Shane Lawson <shane-lawson@users.noreply.github.com>
2020-04-27 14:34:54 -0500

    Initial Commit

---
+++ CovidApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CovidApp
+//
+//  Created by Shane Lawson on 4/27/20.
+//  Copyright © 2020 Shane Lawson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
---
+++ CovidApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CovidApp
+//
+//  Created by Shane Lawson on 4/27/20.
+//  Copyright © 2020 Shane Lawson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
