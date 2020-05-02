62b719e194f5f68df57e1e92caae85a9fcccfbd0
Shane Lawson <shane-lawson@users.noreply.github.com>
2020-05-02 13:53:37 -0500

    Implement question cell filling based on data
    Mark selected choice as correct/incorrect on selection

--- CovidApp/QuizViewController.swift
+++ CovidApp/QuizViewController.swift
@@ -12,6 +12,8 @@ class QuizViewController: UITableViewController {
    
    var tableSections: [QuizTableSection]!
    
+   @IBOutlet var quizTableView: UITableView!
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       
@@ -23,30 +25,75 @@ class QuizViewController: UITableViewController {
    // MARK: - UITableViewDataSource
    
    override func numberOfSections(in tableView: UITableView) -> Int {
-      return 1
+      return tableSections.count
    }
    
    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return 1
+      return tableSections[section].choices.count + 1
    }
    
-//   override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//      return tableSections[section].title
-//   }
+   override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+      return "Question \(section+1)"
+   }
    
    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//      let tableCellData = tableSections[indexPath.section].items[indexPath.row]
-      let cell = tableView.dequeueReusableCell(withIdentifier: "quizCell")!
-//      cell.textLabel?.text = tableCellData.title
-//      cell.detailTextLabel?.text = URL(string: tableCellData.url)?.host
-//      cell.detailTextLabel?.textColor = .systemGray
+      let tableCellData = tableSections[indexPath.section]
+      var cell: UITableViewCell
+      if indexPath.row == 0 {
+         cell = tableView.dequeueReusableCell(withIdentifier: "quizQuestionCell")!
+         cell.textLabel?.numberOfLines = 0
+         cell.textLabel?.font = .systemFont(ofSize: UIFont.systemFontSize * 1.3)
+         cell.textLabel?.text = tableCellData.question
+      } else {
+         cell = tableView.dequeueReusableCell(withIdentifier: "quizQuestionChoiceCell")!
+         cell.textLabel?.numberOfLines = 0
+         cell.textLabel?.attributedText = NSMutableAttributedString(string: tableCellData.choices[indexPath.row - 1])
+         cell.imageView?.image = UIImage(systemName: "circle")
+         cell.imageView?.tintColor = .darkText
+      }
       return cell
    }
    
    // MARK: - UITableViewDelegate
    
-//   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//      let tableCellData = tableSections[indexPath.section].items[indexPath.row]
-//   }
+   override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+      if indexPath.row == 0 {
+         return nil
+      } else {
+         return indexPath
+      }
+   }
+   
+   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      tableView.deselectRow(at: indexPath, animated: true)
+      if let cell = tableView.cellForRow(at: indexPath) {
+         let cellData = tableSections[indexPath.section]
+         if cellData.correctIndex == indexPath.row - 1 {
+            //correct
+            cell.imageView?.image = UIImage(systemName: "checkmark.circle.fill")
+            cell.imageView?.tintColor = .systemGreen
+            let attrString = cell.textLabel?.attributedText
+            cell.textLabel?.attributedText = NSAttributedString(string: attrString!.string, attributes: [
+               .foregroundColor: UIColor.systemGreen,
+            ])
+            //select all other options to mark them incorrect
+            for i in 1...cellData.choices.count {
+               let indPath = IndexPath(row: i, section: indexPath.section)
+               if indPath != indexPath {
+                  quizTableView.delegate?.tableView?(tableView, didSelectRowAt: indPath)
+               }
+            }
+         } else {
+            //incorrect
+            cell.imageView?.image = UIImage(systemName: "xmark.circle.fill")
+            cell.imageView?.tintColor = .systemRed
+            let attrString = cell.textLabel?.attributedText
+            cell.textLabel?.attributedText = NSAttributedString(string: attrString!.string, attributes: [
+               .foregroundColor: UIColor.systemRed,
+               .strikethroughStyle: 1
+            ])
+         }
+      }
+   }
 }
 
